d8700cb261357f711f812e51b57ed28381119a98
EL-QASRY Sami <samielqasry2004@gmail.com>
2025-05-19 10:08:16 +0200

    initialisation du projet

---
+++ src/main/java/com/example/elqasrysami/ElqasrysamiApplication.java
@@ -0,0 +1,13 @@
+package com.example.elqasrysami;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElqasrysamiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElqasrysamiApplication.class, args);
+    }
+
+}
